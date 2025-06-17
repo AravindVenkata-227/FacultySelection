f6203b1519da005c83562aca2121a6a6f4e3647d
Aravind Donthi <aravinddonthi227@gmail.com>
2025-06-17 18:36:42 +0000

    Cross origin request detected from 9003-firebase-studio-1750174899976.cl

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -17,6 +18,9 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  experimental: {
+    allowedDevOrigins: ['https://9003-firebase-studio-1750174899976.cluster-fkltigo73ncaixtmokrzxhwsfc.cloudworkstations.dev'],
   },
 };
 
