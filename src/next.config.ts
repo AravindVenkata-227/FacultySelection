2816d4f28568325e84cecd429295418da4dca58c
Aravind Donthi <aravinddonthi227@gmail.com>
2025-06-19 02:55:12 +0000

    [Data Service] Error ensuring slot document for f1_s1: [Error [FirebaseE

---
+++ src/next.config.ts
@@ -0,0 +1,25 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  allowedDevOrigins: ['https://9003-firebase-studio-1750174899976.cluster-fkltigo73ncaixtmokrzxhwsfc.cloudworkstations.dev'],
+};
+
+export default nextConfig;
